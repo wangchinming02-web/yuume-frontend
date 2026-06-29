8584b0b089ba3f0c4962a535d18b5060d8ef7022
禹平/發芽的人/臭執著仔/發芽的白學家/天羽波堤 <wangchinming02@gmail.com>
2026-06-29 23:08:05 +0800

    login

--- js/admin-config.js
+++ js/admin-config.js
@@ -1,4 +1,10 @@
+const SITE_BASE = '/yuume-frontend';
 const API_BASE = 'https://yuume-backend.onrender.com/api';
+
+function siteUrl(path) {
+  const cleanPath = path.startsWith('/') ? path.slice(1) : path;
+  return `${SITE_BASE}/${cleanPath}`;
+}
 
 function getAdminToken() {
   return localStorage.getItem('admin_token');
@@ -30,5 +36,5 @@ async function checkAdminAuth() {
 
 function redirectToLogin(message) {
   if (message) alert(message);
-  window.location.href = 'login.html';
+  window.location.href = siteUrl('login.html');
 }
